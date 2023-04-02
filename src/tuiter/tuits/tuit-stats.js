b4f5bec49124f194a3aaecafb0368c617e329994
Ada Onyekaba <62957076+adaonyekaba3@users.noreply.github.com>
2023-04-02 15:37:53 -0400

    bye

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -1,53 +1,51 @@
-import React from "react";
+import React from 'react';
 import {useDispatch} from "react-redux";
 import {updateTuitThunk} from "../../services/tuits-thunks";
 
-const TuitStats = ( tuit = {}) => {
+const TuitStats = (
+    tuit = {}) => {
+
     const dispatch = useDispatch();
+
     tuit = tuit.tuit;
+
     return (
-        <div className="d-flex justify-content-between pt-2">
-            <a href="#" className="wd-text-decoration-none text-secondary">
-                <i className="bi bi-chat"/><span
-                className="ps-2">{post.replies}</span>
-            </a>
-            <a href="#" className="wd-text-decoration-none text-secondary">
-                <i className="bi bi-repeat"/><span
-                className="ps-2">{post.retuits}</span>
-            </a>
-            <span
-                className="wd-text-decoration-none text-secondary"
-                onClick={()=> dispatch(
-                    updateTuitThunk(
-                        {
-                            ...post,
-                            likes: post.likes + 1
-                        }
-                    )
-                )}
-            >
-                <i className={`${post.likes > 0 ? "bi bi-heart-fill text-danger" : "bi bi-heart"}`}/><span
-                className="ps-2">{post.likes}</span>
-            </span>
-            <span
-                className="wd-text-decoration-none text-secondary"
-                onClick={()=> dispatch(
-                    updateTuitThunk(
-                        {
-                            ...post,
-                            dislikes: post.dislikes + 1
-                        }
-                    )
-                )}
-            >
-                <i className={`${post.dislikes > 0 ? "bi bi-hand-thumbs-down-fill text-primary"
-                                   : "bi bi-hand-thumbs-down"}`}/><span
-                className="ps-2">{post.dislikes}</span>
-            </span>
-            <a href="#" className="wd-text-decoration-none text-secondary">
-                <i className="bi bi-share"/><span className="ps-2"/>
-            </a>
+        <div className="d-flex justify-content-between mt-2 ms-1 pe-5 mb-1 text-muted">
+            <i className="bi bi-chat">
+<span className="ps-2">
+{tuit.replies}
+</span>
+            </i>
+            <i className="bi bi-shuffle">
+<span className="ps-2">
+{tuit.retuits}
+</span>
+            </i>
+            <div>
+                <i className="bi bi-heart-fill me-2 text-danger"
+                   onClick={() => dispatch(updateTuitThunk({
+                                                               ...tuit,
+                                                               likes: tuit.likes + 1
+                                                           }))}>
+<span className="ps-2">
+{tuit.likes}
+</span>
+                </i>
+            </div>
+            <div>
+                <i className="bi bi-hand-thumbs-down-fill me-2 text-danger"
+                   onClick={() => dispatch(updateTuitThunk({
+                                                               ...tuit,
+                                                               dislikes: tuit.dislikes + 1
+                                                           }))}>
+<span className="ps-2">
+{tuit.dislikes}
+</span>
+                </i>
+            </div>
+            <i className="bi bi-share"/>
         </div>
     );
-};
+}
+
 export default TuitStats;
--- src/tuiter/tuits/tuits-list.js
+++ src/tuiter/tuits/tuits-list.js
@@ -1,26 +1,28 @@
 import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
 import TuitItem from "./tuit-item";
-import {findTuitsThunk} from "../../services/tuits-thunks";
+import { useDispatch, useSelector } from "react-redux";
+import {findTuitsThunk}
+    from "../../services/tuits-thunks";
 
-const TuitList = () => {
-    const {tuits, loading} = useSelector(state => state.tuitsData)
+const TuitsList = () => {
+    const {tuits, loading} = useSelector(
+        (state) => state.tuitsData)
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(findTuitsThunk())
     }, [])
-    return(
-        <ul className="list-group">
+    return (
+        <div className="mt-2 bg-light list-group">
             {
                 loading &&
                 <li className="list-group-item">
                     Loading...
                 </li>
             }
-            {
-                tuits.map(post => <TuitItem key={post._id} post={post}/>)
-            }
-        </ul>
+
+            {tuits.map(tuit => <TuitItem key={tuit.id} tuit={tuit} />)}
+        </div>
     );
-};
-export default TuitList;
+}
+
+export default TuitsList;
