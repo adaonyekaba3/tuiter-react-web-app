c759d9e7602442e199694e43a24174e326e16535
Ada Onyekaba <62957076+adaonyekaba3@users.noreply.github.com>
2023-04-02 15:09:41 -0400

    ignore last change

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -3,54 +3,6 @@ import {useDispatch} from "react-redux";
 import {updateTuitThunk} from "../../services/tuits-thunks";
 
 const TuitStats = (
-    tuit = {}) => {
-
-    const dispatch = useDispatch();
-
-    tuit = tuit.tuit;
-
-    return (
-        <div className="d-flex justify-content-between mt-2 ms-1 pe-5 mb-1 text-muted">
-            <i className="bi bi-chat">
-<span className="ps-2">
-{tuit.replies}
-</span>
-            </i>
-            <i className="bi bi-shuffle">
-<span className="ps-2">
-{tuit.retuits}
-</span>
-            </i>
-            <div>
-                <i className="bi bi-heart-fill me-2 text-danger"
-                   onClick={() => dispatch(updateTuitThunk({
-                                                               ...tuit,
-                                                               likes: tuit.likes + 1
-                                                           }))}>
-<span className="ps-2">
-{tuit.likes}
-</span>
-                </i>
-            </div>
-            <div>
-                <i className="bi bi-hand-thumbs-down-fill me-2 text-danger"
-                   onClick={() => dispatch(updateTuitThunk({
-                                                               ...tuit,
-                                                               dislikes: tuit.dislikes + 1
-                                                           }))}>
-<span className="ps-2">
-{tuit.dislikes}
-</span>
-                </i>
-            </div>
-            <i className="bi bi-share"/>
-        </div>
-    );
-}
-
-export default TuitStats;
-/*
-const TuitStats = (
     {
         post = {
             "topic": "Space",
@@ -114,4 +66,4 @@ const TuitStats = (
         </div>
     );
 };
-export default TuitStats;*/
+export default TuitStats;
